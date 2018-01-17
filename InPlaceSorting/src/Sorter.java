4c36477c15e9b094b1950de73ec2de3ece487db0
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-17 09:23:52 -0500

    fvadfsd

--- InPlaceSorting/src/Sorter.java
+++ InPlaceSorting/src/Sorter.java
@@ -5,5 +5,18 @@ public class Sorter {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public static void BubbleSort(String[]list1)
+	{	
+		int numberofswaps=0;
+		
+	}
+	
+	public static void Swap(int[]arr, int index1, int index2)
+	{
+		int x= arr[index1];
+		arr[index1]=arr[index2];
+		arr[index2]=x;
+	}
 
 }
